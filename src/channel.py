fee715e0cbf7f14fd89b8f240f2a0caf7286ab15
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-08-24 12:53:15 +0700

    implementing Channel class

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,19 @@
+import os
+from src.helpers import printj
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key: str = os.getenv('YT_API_KEY')
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self._youtube = build('youtube', 'v3', developerKey=Channel.api_key)
+        self._channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self._youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
+        printj(channel)
