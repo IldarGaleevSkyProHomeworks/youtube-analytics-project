c15a2150ae667e719770118279defeaa017597ce
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-08-24 14:56:11 +0700

    refactoring channel.py
    - docstrings
    - added http_lib for mocking

--- src/channel.py
+++ src/channel.py
@@ -1,19 +1,33 @@
 import os
-from src.helpers import printj
+import src.helpers
 from googleapiclient.discovery import build
 
 
 class Channel:
-    """Класс для ютуб-канала"""
-
+    """Youtube channel"""
+    http_lib = None
     api_key: str = os.getenv('YT_API_KEY')
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self._youtube = build('youtube', 'v3', developerKey=Channel.api_key)
+        """
+
+        """
+
+        self._youtube = build(
+            serviceName='youtube',
+            version='v3',
+            developerKey=Channel.api_key,
+            http=Channel.http_lib
+        )
+
         self._channel_id = channel_id
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        channel = self._youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
-        printj(channel)
+        """Printing channel info"""
+
+        channel = self._youtube.channels().list(
+            id=self._channel_id,
+            part='snippet,statistics'
+        ).execute()
+
+        src.helpers.printj(channel)
