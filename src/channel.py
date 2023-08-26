4d1ac291ba71bbb0faa4930d91fa25cc5fb0a29a
Ильдар Галеев <ildar-galeev92@mail.ru>
2023-08-26 10:11:30 +0700

    homework2

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,4 @@
+import json
 import os
 import src.helpers
 from googleapiclient.discovery import build
@@ -7,27 +8,76 @@ class Channel:
     """Youtube channel"""
     http_lib = None
     api_key: str = os.getenv('YT_API_KEY')
+    base_url = r'https://www.youtube.com'
+    channel_url = f'{base_url}/channel'
+    _youtube = None
+
+    @classmethod
+    def get_service(cls, reload=False):
+        """
+        returns API service object
+        :param reload: if True - rebuild API service object
+        """
+        if cls._youtube is None or reload:
+            cls._youtube = build(
+                serviceName='youtube',
+                version='v3',
+                developerKey=cls.api_key,
+                http=cls.http_lib
+            )
+        return cls._youtube
 
     def __init__(self, channel_id: str) -> None:
         """
-
+        :param channel_id: channel id
         """
 
-        self._youtube = build(
-            serviceName='youtube',
-            version='v3',
-            developerKey=Channel.api_key,
-            http=Channel.http_lib
-        )
-
+        self._channel_info_raw = None
         self._channel_id = channel_id
 
     def print_info(self) -> None:
         """Printing channel info"""
 
-        channel = self._youtube.channels().list(
+        src.helpers.printj(self.channel_info_raw)
+
+    def update_channel_info(self):
+        """ force update channel info """
+        self._channel_info_raw = Channel.get_service().channels().list(
             id=self._channel_id,
             part='snippet,statistics'
         ).execute()
 
-        src.helpers.printj(channel)
+    @property
+    def channel_info_raw(self):
+        """ full response json """
+        if self._channel_info_raw is None:
+            self.update_channel_info()
+        return self._channel_info_raw
+
+    @property
+    def channel_info(self):
+        """ channel info """
+        return self.channel_info_raw["items"][0]
+
+    @property
+    def title(self):
+        """ channel title """
+        return self.channel_info["snippet"]["title"]
+
+    @property
+    def url(self):
+        """ channel url """
+        return f'{Channel.channel_url}/{self._channel_id}'
+
+    @property
+    def video_count(self):
+        """ video count """
+        return int(self.channel_info["statistics"]["videoCount"])
+
+    def to_json(self, filename):
+        """
+        save channel info to json file
+        :param filename: target file
+        """
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(self.channel_info, file)
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,4 +1,6 @@
 import json
+import os.path
+import shutil
 
 from googleapiclient.http import HttpMock
 from src.channel import Channel
@@ -6,6 +8,7 @@ from pytest import fixture
 
 import src.helpers
 
+FAKE_FS_PATH = "fakefs/"
 TEST_DATA_DIR = "tests/youtube_api_responses"
 
 CHANNEL_LIST_RESPONSE_FILE = f'{TEST_DATA_DIR}/channels_list.json'
@@ -16,8 +19,18 @@ def google_api_mock_Channel():
     http = HttpMock(CHANNEL_LIST_RESPONSE_FILE, {'status': '200'})
     # Channel.api_key = "fake_api_key"
     Channel.http_lib = http
+    Channel.get_service(True)
     yield Channel
     Channel.http_lib = None
+    Channel._youtube = None
+
+
+@fixture()
+def fake_fs():
+    if os.path.exists(FAKE_FS_PATH):
+        shutil.rmtree(FAKE_FS_PATH)
+    os.makedirs(FAKE_FS_PATH, exist_ok=True)
+    yield FAKE_FS_PATH
 
 
 @fixture()
@@ -31,3 +44,28 @@ def test_print_info(google_api_mock_Channel, youtube_api_responses_channels_list
     ch = google_api_mock_Channel("123")
     ch.print_info()
     src.helpers.printj.assert_called_once_with(youtube_api_responses_channels_list)
+
+
+def test_title_prop(google_api_mock_Channel):
+    channel1 = google_api_mock_Channel('')
+    assert channel1.title == 'ChannelName'
+
+
+def test_video_count_prop(google_api_mock_Channel):
+    channel1 = google_api_mock_Channel('')
+    assert channel1.video_count == 5
+
+
+def test_video_url_prop(google_api_mock_Channel):
+    channel1 = google_api_mock_Channel('FAKE_CHANNEL_ID')
+
+    assert channel1.url == f'{channel1.channel_url}/FAKE_CHANNEL_ID'
+
+
+def test_video_to_json(google_api_mock_Channel, fake_fs):
+
+    file_path = os.path.join(fake_fs, 'channel.json')
+    channel1 = google_api_mock_Channel('')
+    channel1.to_json(file_path)
+
+    assert os.path.exists(file_path)
